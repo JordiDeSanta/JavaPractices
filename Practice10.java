163a93907cd0eec74f119291a0d47322f47d8101
Jordi <jordi.bravo.bad@gmail.com>
2021-01-11 17:12:23 -0300

    Warehouse S.A.

---
+++ Practice10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Practice10 {
+    public static void main(String args[]) {
+        // STORE
+                
+        Scanner sc = new Scanner(System.in);
+         
+        // Ask for weight
+        System.out.print("Weight: ");
+        int weight = sc.nextInt();
+    }
+}
